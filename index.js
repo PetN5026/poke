d7d46f92b5bb29470e3864060ff4bd72b3e3f645
Peter <peterng.918@gmail.com>
2022-09-29 20:00:32 -0400

    found url

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('DO OR DIE. LAST CHANCE');
+
+const Express = require('express');
+
+const app = Express();
+const axios = require('axios');
+
+
+async function test(){
+  const ditto = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+  console.log(ditto.data.sprites.front_default)
+}
+
+test();
