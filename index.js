90db5b1416ad0c37cf3806aa90b34ebc3d801161
Jae Shin <shin.jw11@gmail.com>
2022-09-29 20:49:54 -0400

    got the pokemon img to localhost

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-console.log('DO OR DIE. LAST CHANCE');
-
 const Express = require('express');
 
 const app = Express();
 const axios = require('axios');
 
 
-async function test(){
-  const ditto = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
-  console.log(ditto.data.sprites.front_default)
+async function test(poke){
+  const pkm = await axios.get("https://pokeapi.co/api/v2/pokemon/" + poke);
+  //console.log('POKEMON!@!#!@#$%!#!#%!' , pkm.data.sprites.front_default)
+  return pkm.data.sprites.front_default;
 }
 
-test();
+//GET
+app.get('/' , async (req, res) => {
+  const pokemonDitto = await test('ditto');
+  res.send(`<img src=${pokemonDitto}></img>`)
+})
+
+app.get('/:pokemon' , async (req, res) => {
+  const pokeman = await test(req.params.pokemon)
+  res.send(`<img src=${pokeman}></img>`)
+})
+
+//LISTEN
+const PORT = 3000;
+app.listen(PORT, (req, res) => {
+  console.log(`LISTENING ON ${PORT}`)
+})
